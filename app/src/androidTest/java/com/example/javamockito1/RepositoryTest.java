f3aa65369a40b900a7be688a7bf6dbbc68176243
MkItSimple <choanania1@gmail.com>
2020-06-05 06:59:30 +0800

    sample-2

--- app/src/androidTest/java/com/example/javamockito1/RepositoryTest.java
+++ app/src/androidTest/java/com/example/javamockito1/RepositoryTest.java
@@ -2,35 +2,44 @@ package com.example.javamockito1;
 
 import android.util.Log;
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.MutableLiveData;
+
 import com.example.javamockito1.repository.Repository;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RepositoryTest {
 
-    @Mock
+    @Spy
     Repository repository;
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule =
+            new InstantTaskExecutorRule();
 
     @Before
     public void setUp() throws Exception {
-        //MockitoAnnotations.initMocks(Repository.class);
-        //repository = Repository.class;
     }
 
     @Test
     public void getData() {
-        //Repository repository = mock(Repository.class);
-        when(repository.getData("choreyn")).thenReturn("yess!!!");
-        Log.d("Choreyn", repository.getData("choreyn"));
-
+        when(repository.getMutableLiveData("cho", "cho")).thenReturn(repository.mutableLiveData);
+        Log.d("Mockito",""+ repository.mutableLiveData.getValue());
     }
 }
--- app/src/main/java/com/example/javamockito1/repository/Repository.java
+++ app/src/main/java/com/example/javamockito1/repository/Repository.java
@@ -1,8 +1,15 @@
 package com.example.javamockito1.repository;
 
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
 public class Repository {
 
-    public String getData(String cho){
-        return cho;
+    public MutableLiveData<String> mutableLiveData = new MutableLiveData<>();
+
+    public MutableLiveData<String> getMutableLiveData(String name, String pass) {
+        mutableLiveData.setValue("name is "+name+" pass is "+pass);
+        return mutableLiveData;
     }
 }
